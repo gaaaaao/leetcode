32fd7a46217bf861c9bc9ae084db906ec9f5976a
gaaaaao <gao.yan.gg1994@gmail.com>
2017-08-18 10:48:29 -0400

    Create leetcode49.cpp

---
+++ leetcode49.cpp
@@ -0,0 +1,12 @@
+class solution
+{
+public:
+	vector<vector<string>> groupAnagrams(vector<string> & strs)
+	{
+		unordered_map<string, vector<string>> hash;
+		for(int i = 0;i<strs.size();i++)
+		{
+			string temp = strs[i]
+		}
+	}
+}
